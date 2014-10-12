65811b15c3bcd29ea06719b69476b823c85b429f
kukubaczek <kukubaczek@mac.local>
2014-10-12 10:25:22 +0200

    Kolejne AntyCheaty
    [+] Gdy gracz napisze coś na chacie i będzie „shiftował” wykryje to
    plugin i wyśle o tym wiadomość na administracji!

--- src/com/gmail/kukubaczek/SmartAdmin/events/OnChat.java
+++ src/com/gmail/kukubaczek/SmartAdmin/events/OnChat.java
@@ -1,5 +1,6 @@
 package com.gmail.kukubaczek.SmartAdmin.events;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,6 +18,9 @@ public class OnChat implements Listener {
     			p.sendMessage(Main.getTag("error") + " §fChat jest aktualnie wyłączony!");
     			e.setCancelled(true);
     		}
+    	}
+    	if(e.getPlayer().isSneaking()){
+    		Bukkit.broadcast(Main.getTag("AC") + " §fGracz §a" + p.getName() + " §fnapisał wiadomość na §achacie§f, gdy §asię skradał§f!", "SmartAdmin.AntyCheat");
     	}
     }
 }
