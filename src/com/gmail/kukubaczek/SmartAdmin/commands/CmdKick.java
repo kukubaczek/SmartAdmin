b592a6ab163c2188f0664af1f3e0ce70f1700725
JuniorJPDJ <juniorjpdj@interia.pl>
2014-10-13 17:40:10 +0200

    Posprzatalem troche zbednych importow

--- src/com/gmail/kukubaczek/SmartAdmin/commands/CmdKick.java
+++ src/com/gmail/kukubaczek/SmartAdmin/commands/CmdKick.java
@@ -1,11 +1,9 @@
 package com.gmail.kukubaczek.SmartAdmin.commands;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import com.gmail.kukubaczek.SmartAdmin.Main;
 
--- src/com/gmail/kukubaczek/SmartAdmin/events/OnInvClick.java
+++ src/com/gmail/kukubaczek/SmartAdmin/events/OnInvClick.java
@@ -7,7 +7,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import com.gmail.kukubaczek.SmartAdmin.Main;
