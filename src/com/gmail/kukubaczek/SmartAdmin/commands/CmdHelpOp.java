65c7153667f9f00246d76dff8441778838a1bfe0
kukubaczek <kukubaczek1@gmail.com>
2014-10-14 12:42:22 +0200

    Dodano komendę
    [+] HelpOp
    [-] Błędy

---
+++ src/com/gmail/kukubaczek/SmartAdmin/commands/CmdHelpOp.java
@@ -0,0 +1,33 @@
+package com.gmail.kukubaczek.SmartAdmin.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import com.gmail.kukubaczek.SmartAdmin.Main;
+import com.gmail.kukubaczek.SmartAdmin.functions.KickAll;
+
+public class CmdHelpOp implements CommandExecutor {
+	  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		    if(!sender.hasPermission("SmartAdmin.helpop")){
+		      sender.sendMessage("Nie masz permisji! c:");
+		      return true;
+		    }
+		    
+		    if(args.length == 0){
+		    	sender.sendMessage(Main.getTag("error") + " §fWpisz wiadomość! §a/§fhelpop §a<§fwiadomosc§a>§f!");
+		    }else{
+			    	
+			    String msg = "";
+			    for(int i = 0; i < args.length; i++){
+			      msg += args[i] + " ";
+			    }
+	
+			    Bukkit.broadcast("§4[§cHelpOp§4] §7" + sender.getName() + "§b >> §f" + msg, "SmartAdmin.*");
+	
+			    return true;
+		    }
+			return false;
+	  }
+	}
