1e9191dd46e8bf7e2bd837cfa2284819b1742ca9
kukubaczek <kukubaczek1@gmail.com>
2014-10-16 14:28:34 +0200

    Znowu poprawiono komende -.-

--- src/com/gmail/kukubaczek/SmartAdmin/commands/CmdInvSee.java
+++ src/com/gmail/kukubaczek/SmartAdmin/commands/CmdInvSee.java
@@ -8,7 +8,7 @@ import org.bukkit.entity.Player;
 import com.gmail.kukubaczek.SmartAdmin.Main;
 import com.gmail.kukubaczek.SmartAdmin.functions.InvSee;
 
-public class CmdInvSee implements CommandExecutor {{
+public class CmdInvSee implements CommandExecutor {
 	  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		    if(!sender.hasPermission("SmartAdmin.InvSee")){
 		      sender.sendMessage("Nie masz permisji! c:");
