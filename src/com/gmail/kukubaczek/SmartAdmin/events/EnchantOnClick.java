816ae7f6d44566d9d3e298a281f93fe9c4a6e94e
kukubaczek <kukubaczek1@gmail.com>
2014-10-19 11:15:36 +0200

    Ogarnięto enchantowanie
    Jeszze errory sypią ale zaraz się usunie D:

--- src/com/gmail/kukubaczek/SmartAdmin/events/EnchantOnClick.java
+++ src/com/gmail/kukubaczek/SmartAdmin/events/EnchantOnClick.java
@@ -6,13 +6,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryType.SlotType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import com.gmail.kukubaczek.SmartAdmin.Main;
-import com.gmail.kukubaczek.SmartAdmin.MainMenuCreator;
 
 public class EnchantOnClick implements Listener {
 	
@@ -34,14 +31,34 @@ public class EnchantOnClick implements Listener {
 				        		Bukkit.broadcastMessage("3");
 				        	 if(clicked.getDisplayName().equals("§bEfficiency")){
 				        		 Bukkit.broadcastMessage("4");
-				        		 int lvl = cursor.getEnchantmentLevel(Enchantment.DIG_SPEED);
-				        		 if(event.isLeftClick()){
-				        			 ItemMeta met = cursor.getItemMeta();
-				        			 met.addEnchant(Enchantment.DIG_SPEED, lvl + 1, true);
-				        			 Bukkit.broadcastMessage("5");
-				        		 }else if(event.isRightClick()){
-				        			 Bukkit.broadcastMessage("6");
-				        		 }
+				        		 if(event.getCursor() != null){
+					        		 int lvl = cursor.getEnchantmentLevel(Enchantment.DIG_SPEED);
+					        		 if(event.isShiftClick()){
+					        			 ItemMeta met = cursor.getItemMeta();
+					       				 met.removeEnchant(Enchantment.DIG_SPEED);
+					        			 cursor.setItemMeta(met);
+					        			 event.setCursor(cursor);
+					        			 Bukkit.broadcastMessage("7");
+					        		 }else{
+						        		 if(event.isLeftClick()){
+						        			 ItemMeta met = cursor.getItemMeta();
+						        			 met.addEnchant(Enchantment.DIG_SPEED, lvl + 1, true);
+						        			 cursor.setItemMeta(met);
+						        			 event.setCursor(cursor);
+						        			 Bukkit.broadcastMessage("5");
+						        		 }else if(event.isRightClick()){
+						        			 ItemMeta met = cursor.getItemMeta();
+						        			 if(lvl == 1){
+						        				 met.removeEnchant(Enchantment.DIG_SPEED);
+						        			 }else{
+							        			 met.addEnchant(Enchantment.DIG_SPEED, lvl - 1, false); 
+						        			 }
+						        			 cursor.setItemMeta(met);
+						        			 event.setCursor(cursor);
+						        			 Bukkit.broadcastMessage("6");
+						        		 }
+					        		 }
+				        	 	}
 				        	}
 			        	}
 	        	}
