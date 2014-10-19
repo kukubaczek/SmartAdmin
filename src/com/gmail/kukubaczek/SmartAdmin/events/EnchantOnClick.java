8c9ea8ee7ed34f504676ce9c0c2a2682859568c4
JuniorJPDJ <juniorjpdj@interia.pl>
2014-10-19 11:40:25 +0200

    Fixy :D

--- src/com/gmail/kukubaczek/SmartAdmin/events/EnchantOnClick.java
+++ src/com/gmail/kukubaczek/SmartAdmin/events/EnchantOnClick.java
@@ -12,59 +12,55 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 
 public class EnchantOnClick implements Listener {
-	
-	  @EventHandler
-	  public void onInventoryClick(InventoryClickEvent event) {
-	    Player player = (Player) event.getWhoClicked();
-	    Inventory inventory = event.getInventory();
-	    if((inventory.getName().equals("§aS§fmart§bA§fdmin: §eEnchant")) && (event.getCurrentItem() != null)){
-	      if(player.hasPermission("SmartAdmin.*")){
 
-	        ItemMeta clicked = event.getCurrentItem().getItemMeta();
-	        ItemStack cursor = event.getCursor();
-	        if(clicked == null) return;
-	        if(clicked.hasDisplayName()){
-	        	if(cursor.getType() != null){
-		        	if(event.getRawSlot() <= 53){
-			      	      event.setCancelled(true);
-			        		Bukkit.broadcastMessage("1");
-				        		Bukkit.broadcastMessage("3");
-				        	 if(clicked.getDisplayName().equals("§bEfficiency")){
-				        		 Bukkit.broadcastMessage("4");
-				        		 if(event.getCursor() != null){
-					        		 int lvl = cursor.getEnchantmentLevel(Enchantment.DIG_SPEED);
-					        		 if(event.isShiftClick()){
-					        			 ItemMeta met = cursor.getItemMeta();
-					       				 met.removeEnchant(Enchantment.DIG_SPEED);
-					        			 cursor.setItemMeta(met);
-					        			 event.setCursor(cursor);
-					        			 Bukkit.broadcastMessage("7");
-					        		 }else{
-						        		 if(event.isLeftClick()){
-						        			 ItemMeta met = cursor.getItemMeta();
-						        			 met.addEnchant(Enchantment.DIG_SPEED, lvl + 1, true);
-						        			 cursor.setItemMeta(met);
-						        			 event.setCursor(cursor);
-						        			 Bukkit.broadcastMessage("5");
-						        		 }else if(event.isRightClick()){
-						        			 ItemMeta met = cursor.getItemMeta();
-						        			 if(lvl == 1){
-						        				 met.removeEnchant(Enchantment.DIG_SPEED);
-						        			 }else{
-							        			 met.addEnchant(Enchantment.DIG_SPEED, lvl - 1, false); 
-						        			 }
-						        			 cursor.setItemMeta(met);
-						        			 event.setCursor(cursor);
-						        			 Bukkit.broadcastMessage("6");
-						        		 }
-					        		 }
-				        	 	}
-				        	}
-			        	}
-	        	}
+  @SuppressWarnings("deprecation")
+  @EventHandler
+  public void onInventoryClick(InventoryClickEvent event) {
+    Player player = (Player) event.getWhoClicked();
+    Inventory inventory = event.getInventory();
+    if((inventory.getName().equals("§aS§fmart§bA§fdmin: §eEnchant")) && (event.getCurrentItem() != null)){
+      if(player.hasPermission("SmartAdmin.*")){
 
-	        }
-	      }
-	    }
-	  }
+        ItemMeta clicked = event.getCurrentItem().getItemMeta();
+        ItemStack cursor = event.getCursor();
+        if(clicked == null) return;
+        if(clicked.hasDisplayName()){
+          if(cursor.getType() != null){
+            if(event.getRawSlot() <= 53){
+              event.setCancelled(true);
+              Bukkit.broadcastMessage("1");
+              Bukkit.broadcastMessage("3");
+              if(clicked.getDisplayName().equals("§bEfficiency")){
+                Bukkit.broadcastMessage("4");
+                if(event.getCursor() != null){
+                  ItemMeta meta = cursor.getItemMeta();
+                  int lvl = cursor.getEnchantmentLevel(Enchantment.DIG_SPEED);
+                  if(event.isShiftClick()){
+                    meta.removeEnchant(Enchantment.DIG_SPEED);
+                    cursor.setItemMeta(meta);
+                    event.setCursor(cursor);
+                    Bukkit.broadcastMessage("7");
+                  }else if(event.isLeftClick()){
+                    meta.addEnchant(Enchantment.DIG_SPEED, lvl + 1, true);
+                    cursor.setItemMeta(meta);
+                    event.setCursor(cursor);
+                    Bukkit.broadcastMessage("5");
+                  }else if(event.isRightClick()){
+                    if(lvl == 1){
+                      meta.removeEnchant(Enchantment.DIG_SPEED);
+                    }else{
+                      meta.addEnchant(Enchantment.DIG_SPEED, lvl - 1, false); 
+                    }
+                    cursor.setItemMeta(meta);
+                    event.setCursor(cursor);
+                    Bukkit.broadcastMessage("6");
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
--- src/com/gmail/kukubaczek/SmartAdmin/events/OnRightClick.java
+++ src/com/gmail/kukubaczek/SmartAdmin/events/OnRightClick.java
@@ -12,11 +12,15 @@ public class OnRightClick implements Listener{
   @EventHandler
   public void onPlayerInteract(PlayerInteractEvent event) {
     Player player = (Player) event.getPlayer();
-    if(event.getItem() != null && event.getItem().hasItemMeta() && event.getItem().getItemMeta().getDisplayName().equals("§bSmartAdmin") && (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)){
-      if(player.hasPermission("smartadmin.open")){
-        MainMenuCreator.openInv(player);
-      }else{
-        player.getInventory().remove(event.getItem());
+    if(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK){
+      if(event.getItem() != null){
+        if(event.getItem().hasItemMeta() && event.getItem().getItemMeta().getDisplayName().equals("§bSmartAdmin")){
+          if(player.hasPermission("smartadmin.open")){
+            MainMenuCreator.openInv(player);
+          }else{
+            player.getInventory().remove(event.getItem());
+          }
+        }
       }
     }
   }
