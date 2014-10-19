a95d8be9514fdc23f8c6de6efa6f58a570308525
JuniorJPDJ <juniorjpdj@interia.pl>
2014-10-19 12:14:44 +0200

    drobny fix na szybko

--- src/com/gmail/kukubaczek/SmartAdmin/events/EnchantOnClick.java
+++ src/com/gmail/kukubaczek/SmartAdmin/events/EnchantOnClick.java
@@ -1,6 +1,5 @@
 package com.gmail.kukubaczek.SmartAdmin.events;
 
-import org.bukkit.Bukkit;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -28,10 +27,10 @@ public class EnchantOnClick implements Listener {
           if(cursor.getType() != null){
             if(event.getRawSlot() <= 53){
               event.setCancelled(true);
-              Bukkit.broadcastMessage("1");
-              Bukkit.broadcastMessage("3");
+              player.sendMessage("1");
+              player.sendMessage("3");
               if(clicked.getDisplayName().equals("§bEfficiency")){
-                Bukkit.broadcastMessage("4");
+                player.sendMessage("4");
                 if(event.getCursor() != null){
                   ItemMeta meta = cursor.getItemMeta();
                   int lvl = cursor.getEnchantmentLevel(Enchantment.DIG_SPEED);
@@ -39,21 +38,21 @@ public class EnchantOnClick implements Listener {
                     meta.removeEnchant(Enchantment.DIG_SPEED);
                     cursor.setItemMeta(meta);
                     event.setCursor(cursor);
-                    Bukkit.broadcastMessage("7");
+                    player.sendMessage("7");
                   }else if(event.isLeftClick()){
                     meta.addEnchant(Enchantment.DIG_SPEED, lvl + 1, true);
                     cursor.setItemMeta(meta);
                     event.setCursor(cursor);
-                    Bukkit.broadcastMessage("5");
+                    player.sendMessage("5");
                   }else if(event.isRightClick()){
                     if(lvl == 1){
                       meta.removeEnchant(Enchantment.DIG_SPEED);
                     }else{
-                      meta.addEnchant(Enchantment.DIG_SPEED, lvl - 1, false); 
+                      meta.addEnchant(Enchantment.DIG_SPEED, lvl - 1, true); 
                     }
                     cursor.setItemMeta(meta);
                     event.setCursor(cursor);
-                    Bukkit.broadcastMessage("6");
+                    player.sendMessage("6");
                   }
                 }
               }
